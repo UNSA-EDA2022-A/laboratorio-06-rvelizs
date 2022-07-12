1c02d3f289542efed4da296ddcec4c9145c912d7
rvelizs <88206023+rvelizs@users.noreply.github.com>
2022-07-12 13:21:44 -0500

    Update Exercise1.java

--- src/main/java/com/example/project/Exercise1.java
+++ src/main/java/com/example/project/Exercise1.java
@@ -19,9 +19,52 @@ public class Exercise1 {
         
     }
 
-
     public <T extends Comparable<T>> boolean bstSimilares(BST<T> a1, BST<T> a2){
+        // CÁLCULO DE ALTURAS
+		int leftAltura = altura(a1.root);
+		int rightAltura = altura(a1.root);
 
-        return false;
+		// LO QUE SE RETORNA
+		boolean retorno = true;
+
+		// SI NO TIENEN MISMA ALTURA
+		if (leftAltura != rightAltura)
+			retorno = false;
+
+		// ARREGLOS DEL TAMAÑO DE LA ALTURA DE CADA SUBARBOL
+		int[] sumaAlturasLeft = new int[leftAltura];
+		int[] sumaAlturasRight = new int[leftAltura];
+
+		// PROCESO POR LA IZQUIERDA Y DERECHA
+		contador(a1.root, leftAltura, sumaAlturasLeft);
+		contador(a2.root, rightAltura, sumaAlturasRight);
+
+		// SI AMBOS ARREGLOS SON IGUALES
+		for (int i = 0; i < sumaAlturasLeft.length; i++) {
+			if (sumaAlturasLeft[i] != sumaAlturasRight[i])
+				retorno = false;
+		}
+
+		return retorno;
     }
+    
+    protected <T extends Comparable<T>> int altura(Node<T> a) {
+		if (a == null)
+			return 0;
+		else
+			// ALTURA POR UNO (NO SE CUENTA UN NODO)
+			return 1 + Math.max(altura(a.left), altura(a.right));
+	}
+    
+    protected <T extends Comparable<T>> void contador(Node<T> t, int altura, int[] tabla) {
+		if (t == null || altura == 0)
+			return;
+
+		// INCREMENTA SI EXISTE NODO IZQUIERDO O DERECHO
+		tabla[altura - 1]++;
+
+		// RECURSIVO POR LA IZQUIERDA Y DERECHA
+		contador(t.left, altura - 1, tabla);
+		contador(t.right, altura - 1, tabla);
+	}
 }
