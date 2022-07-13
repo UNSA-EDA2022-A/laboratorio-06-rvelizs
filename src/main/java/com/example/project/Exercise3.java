b9ef719fcd2d73fcd032a4a67f06db6b7971c0a5
rvelizs <88206023+rvelizs@users.noreply.github.com>
2022-07-12 23:42:28 -0500

    Update Exercise3.java

--- src/main/java/com/example/project/Exercise3.java
+++ src/main/java/com/example/project/Exercise3.java
@@ -16,10 +16,28 @@ public class Exercise3 {
         System.out.print(obj.bstEstrictamenteBinario(tree));
         
     }
-
-
+    
+    // PÚBLICO
     public <T extends Comparable<T>> boolean bstEstrictamenteBinario(BST<T> a){
-
-        return false;
+        return bstEstrictamenteBinario(a.root);
     }
+    
+    protected <T extends Comparable<T>> boolean bstEstrictamenteBinario(Node<T> node) {
+		// SI EL ARBOL ESTÁ VACÍO
+		if (node == null)
+			return true;
+
+		// SI ES NODO HOJA
+		else if (node.left == null && node.right == null)
+			return true;
+        
+		// SI NINGUNO LADO ES NULO ES ESTRICTAMENTE BST
+		else if (node.left != null && node.right != null) {
+			System.out.println("ASdsa");
+			return bstEstrictamenteBinario(node.left) && bstEstrictamenteBinario(node.right);
+		}
+
+		// EN CASO DE QUE UN LADO TENGA NULL Y EL OTRO NO
+		return false;
+	}
 }
